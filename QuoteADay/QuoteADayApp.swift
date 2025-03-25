55a2cfa628b37488fc49725e5a0eb35ef72f06cc
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-25 11:56:51 +0300

    Initial Commit

---
+++ QuoteADay/QuoteADayApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteADayApp.swift
+//  QuoteADay
+//
+//  Created by mehmet Çelik on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteADayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
